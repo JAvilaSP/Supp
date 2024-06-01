435fd52a16aba667d938e66aa28bca9c3699cf9e
JAvilaSP <119306221+JAvilaSP@users.noreply.github.com>
2024-06-02 00:47:47 +0200

    Conexión con MongoDB

--- resources/js/Pages/SuppIndex.vue
+++ resources/js/Pages/SuppIndex.vue
@@ -1,12 +1,12 @@
 <template>
 
     <Head title=""></Head>
-    <p>a</p>
-    <Calendar v-model="date" />
+    <p>{{ $page.props.auth.user.name }}, se bienvenido a tu futuro proyecto.</p>
+
 </template>
 
 <script setup>
 import { Head, Link } from '@inertiajs/vue3';
-import Calendar from 'primevue/calendar';
+
 
 </script>
