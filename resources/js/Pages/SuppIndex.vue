e3c97734d8d318e4fc4ee1df6ac5a6d2d80442f8
JAvilaSP <119306221+JAvilaSP@users.noreply.github.com>
2024-06-01 20:17:54 +0200

    Added Primevue and MongoDB dependencies

--- resources/js/Pages/SuppIndex.vue
+++ resources/js/Pages/SuppIndex.vue
@@ -1,8 +1,12 @@
 <template>
+
     <Head title=""></Head>
+    <p>a</p>
+    <Calendar v-model="date" />
 </template>
 
 <script setup>
 import { Head, Link } from '@inertiajs/vue3';
+import Calendar from 'primevue/calendar';
 
 </script>
